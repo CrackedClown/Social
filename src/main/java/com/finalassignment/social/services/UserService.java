d2f4b6c5b7c00ee8c661e1946f360e744f29b73b
CrackedClown <harshitagrawal0397@gmail.com>
2019-08-27 23:40:37 +0530

    Add Logs in User Service

--- src/main/java/com/finalassignment/social/services/UserService.java
+++ src/main/java/com/finalassignment/social/services/UserService.java
@@ -4,11 +4,13 @@ import com.finalassignment.social.exceptions.UserAlreadyExistsException;
 import com.finalassignment.social.exceptions.UserNotFoundException;
 import com.finalassignment.social.models.User;
 import com.finalassignment.social.repositories.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class UserService {
     UserRepository userRepository;
@@ -19,27 +21,24 @@ public class UserService {
     }
 
     public User findUserById(Integer id) throws UserNotFoundException {
+        log.debug("Finding A User, In UserService");
         return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User Not Found"));
     }
 
-    //Manually checking username exists or not because unique keyboard is not working for some reason!
     public User createUser(User user) throws UserAlreadyExistsException {
+        log.debug("Creating a User, In UserService");
         User tempUser = userRepository.findByUsername(user.getUsername());
         System.out.println(tempUser);
-       /* List<User> userList = userRepository.findAll();
-        for(User tempUser : userList){
-            if(user.getUsername().equals(tempUser.getUsername())){
-                throw new UserAlreadyExistsException("Username Already Exists");
-            }
-        }*/
         return userRepository.save(user);
     }
 
     public void removeUserById(Integer id) {
+        log.debug("Removing a User, In UserService");
         userRepository.deleteById(id);
     }
 
     public User updateUser(User user, Integer id) throws UserNotFoundException {
+        log.debug("Updating User, In UserService");
         User tempUser = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("UserNotFound"));
         tempUser.setUsername(user.getUsername());
         tempUser.setPassword(user.getPassword());
@@ -48,6 +47,7 @@ public class UserService {
     }
 
     public List<User> getAllUsers() {
+        log.debug("Getting All Users, In UserService");
         return userRepository.findAll();
     }
 }
