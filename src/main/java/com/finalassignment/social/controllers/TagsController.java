9e1d03e4fc1efda90e2ea8fcc6b3ecbe6cc0f697
CrackedClown <harshitagrawal0397@gmail.com>
2019-08-27 23:09:52 +0530

    Add Logs in Tags Controller

--- src/main/java/com/finalassignment/social/controllers/TagsController.java
+++ src/main/java/com/finalassignment/social/controllers/TagsController.java
@@ -3,6 +3,7 @@ package com.finalassignment.social.controllers;
 import com.finalassignment.social.models.Post;
 import com.finalassignment.social.models.Tags;
 import com.finalassignment.social.services.TagsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Set;
 
+@Slf4j
 @RestController
 public class TagsController {
     TagsService tagsService;
@@ -26,16 +28,19 @@ public class TagsController {
 
     @GetMapping("/users/{id}/tags")
     public ResponseEntity<List<Tags>> getAllTags() {
+        log.debug("Getting All Tags, In TagsController");
         return ResponseEntity.status(HttpStatus.OK).body(tagsService.getAllTags());
     }
 
     @GetMapping("/users/{id}/tags/{tagName}")
     public ResponseEntity<Set<Post>> getPostsByTag(@PathVariable String tagName) {
+        log.debug("Getting Posts by Tag, In TagsController");
         return ResponseEntity.status(HttpStatus.OK).body(tagsService.getPostsByTag(tagName));
     }
 
     @DeleteMapping("/users/{id}/tags/{tag_id}")
     public ResponseEntity<Void> removeTagById(@PathVariable Integer tag_id){
+        log.debug("Removing Tag by ID, In TagsController");
         tagsService.removeTagById(tag_id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
